b0d39cc5c47872ca0b6169741f8abb666f85a504
manthansubhash01 <manthanziman20@gmail.com>
2025-04-15 22:11:39 +0530

    updated app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth",require("./routes/authRoute"));
